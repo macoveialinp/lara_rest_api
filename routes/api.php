82db56bd5c1349f83596ee7a185c9cd9831fb4ef
Alin M <macoveialinp@gmail.com>
2025-01-16 01:00:35 +0000

    Album CRUD - done

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AlbumController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('album', AlbumController::class);
